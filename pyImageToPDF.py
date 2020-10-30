96da10a32f4c2b4a561dcc25d44f9bad9808875f
jwhitlow45 <46979583+jwhitlow45@users.noreply.github.com>
2020-10-30 13:51:06 -0400

    Added image selection functionality

--- pyImageToPDF.py
+++ pyImageToPDF.py
@@ -1,23 +1,44 @@
-import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+from tkinter import filedialog
 
 # Globals
+supportedFileTypes = (("JPEG Image",".jpeg"),
+                      ("PNG Image", ".png"),
+                      ("BMP Image", ".bmp"),
+                      ("All Files", "."))
 
+class MainWindow(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
 
-class MainWindow( Frame ):
-    def __init__( self ):
-        tk.Frame.__init__(self)
-        self.pack()
+    def init_window(self):
         self.master.title("Image to PDF")
+        self.pack(fill=BOTH, expand=1)
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+        filetab = Menu(menu)
+        filetab.add_command(label="Open...", command=self.open_file)
+        filetab.add_command(label="Exit", command=self.client_exit)
+        menu.add_cascade(label="File", menu=filetab)
+        
 
-        self.button1 = Button( self, text = "CLICK HERE", width = 25,
+        """self.button1 = Button( self, text = "CLICK HERE", width = 25,
                                command = self.new_window )
-        self.button1.grid( row = 0, column = 1, columnspan = 2, sticky = W+E+N+S )
-    def new_window(self):
-        self.newWindow = karl2()
+        self.button1.grid( row = 0, column = 1, columnspan = 2, sticky = W+E+N+S )"""
 
-def main(): 
-    MainWindow().mainloop()
-if __name__ == '__main__':
-    main()
+    def open_file(self):
+        filename = filedialog.askopenfilename(initialdir = "/",
+                                              title = "Select a File",
+                                              filetypes = supportedFileTypes)
+        print(filename)
+
+    def client_exit(self):
+        exit()
+        
+
+root = Tk()
+root.geometry("400x300")
+app = MainWindow(root)
+root.mainloop()
